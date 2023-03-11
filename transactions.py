dc028f2fac6d707058e22c31029f29bdc7e5baca
Daniel Ortiz <thisisdanielortiz@gmail.com>
2023-03-10 18:07:53 -0600

    Generate a random transaction date

--- transactions.py
+++ transactions.py
@@ -7,3 +7,8 @@ def get_transaction_import(min: int, max: int) -> float:
     rounding_decimal = round(random_decimal, 2)
     return rounding_decimal
 
+
+def get_transaction_date(min_montn: int, max_month: int, min_day: int, max_day: int, year=2023) -> str:
+    date = datetime.date(year=year, month=random.randint(min_montn, max_month), day=random.randint(min_day, max_day))
+    return date.strftime("%m/%d/%Y")
+    
