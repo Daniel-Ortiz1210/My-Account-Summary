49fbed06889ab5411bfc6701d4cc7a54451c1354
Daniel Ortiz <thisisdanielortiz@gmail.com>
2023-03-12 16:31:36 -0600

    define Account and Transaction models with peewee

--- src/database/models/models.py
+++ src/database/models/models.py
@@ -1,2 +1,13 @@
 from src.database.models.base import BaseModel
-from peewee import CharField, IntegerField
+from peewee import CharField, IntegerField, DateField, DecimalField, ForeignKeyField
+
+
+class Account(BaseModel):
+    account_number = CharField(unique=True, null=False, max_length=16)
+
+
+class Transaction(BaseModel):
+    id = CharField(primary_key=True, max_length=13)
+    date = DateField(formats=["%m/%d/%Y"])
+    transaction = DecimalField(decimal_places=2)
+    account_number = ForeignKeyField(model=Account, field=Account.account_number, backref="transactions")
