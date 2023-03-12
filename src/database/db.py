4d1e7ba11b488454ae735f871c5515e868b61297
Daniel Ortiz <thisisdanielortiz@gmail.com>
2023-03-12 04:58:25 -0600

    database package

---
+++ src/database/db.py
@@ -0,0 +1,20 @@
+from peewee import PostgresqlDatabase
+
+
+db_params = {
+    "name": "transactions",
+    "host": "database-1.c5niavzkvagt.us-east-1.rds.amazonaws.com",
+    "user": "postgres",
+    "password": "aZf5gT6f2KVb001"
+    }
+
+psql_db = PostgresqlDatabase(
+    db_params.get("name"), 
+    user=db_params.get("user"), 
+    password=db_params.get("password"), 
+    host=db_params.get("host")
+    )
+
+def init_db():
+    from src.database.models.models import *
+    
