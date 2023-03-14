1cc72ecb714c303ac6f14855d61fa635515283ed
Daniel Ortiz <thisisdanielortiz@gmail.com>
2023-03-13 19:16:24 -0600

    modify pkg name; from transactions -> utils

---
+++ src/utils/csv_gen.py
@@ -0,0 +1,48 @@
+from src.utils.transactions_builders import build_transaction
+from src.schemas.schemas import TransactionsSchema
+from src.database.models.models import Transaction
+
+import csv
+
+
+def insert_transactions_to_db(account):
+    
+    with open(f"csv/{account}.csv", newline="") as csv_file:
+        csv_file = list(csv.DictReader(csv_file))
+        for row in list(csv_file):
+            row["account_number"] = account
+        transactions_list = TransactionsSchema(transactions=list(csv_file))
+
+    Transaction.insert_many(transactions_list.dict()["transactions"]).execute()
+        
+
+def create_csv_file(func):
+    
+    def wrapper(*args, **kwargs):
+        import uuid
+
+        result = func(*args, **kwargs)
+        account = args[0]
+        with open(f"csv/{account}.csv", "w", newline="") as csv_file:
+            csv_writer = csv.writer(csv_file)
+
+            for row in result:
+                csv_writer.writerow(row)
+        
+        insert_transactions_to_db(account)
+
+        return csv_file
+
+    return wrapper
+
+
+@create_csv_file
+def build_transactions_file(account, transaction_builder=build_transaction):
+    headers = ["Id", "Date", "Transaction"]
+
+    csv_body = [headers]
+
+    for _ in range(30):
+        csv_body.append(transaction_builder())
+
+    return csv_body
---
+++ src/utils/transactions_builders.py
@@ -0,0 +1,24 @@
+import datetime
+import uuid
+import random
+
+
+def get_transaction_import(min: int, max: int) -> float:
+    random_decimal = random.uniform(-60, 60)
+    rounding_decimal = round(random_decimal, 2)
+    return rounding_decimal
+
+
+def get_transaction_date(min_montn: int, max_month: int, min_day: int, max_day: int, year=2023) -> str:
+    date = datetime.date(year=year, month=random.randint(min_montn, max_month), day=random.randint(min_day, max_day))
+    return date.strftime("%m/%d/%Y")
+    
+
+def build_transaction() -> list:
+    transaction = list()
+
+    transaction.append(uuid.uuid4().__str__()[:13])
+    transaction.append(get_transaction_date(min_montn=3, max_month=6, min_day=1, max_day=30))
+    transaction.append(get_transaction_import(min=-60, max=60))
+
+    return transaction
