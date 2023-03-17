6f46ab9b35221628fa4f435edec4625d03034e50
Daniel Ortiz <thisisdanielortiz@gmail.com>
2023-03-17 01:10:23 -0600

    uding Secrets Manager

--- src/database/db.py
+++ src/database/db.py
@@ -1,11 +1,45 @@
 from peewee import PostgresqlDatabase
 import os
+import boto3
+from botocore.exceptions import ClientError
 
 
-db_host = os.environ.get("DB_HOST", "database-1.c5niavzkvagt.us-east-1.rds.amazonaws.com")
-db_name = os.environ.get("DB_NAME", "transactions")
-db_password = os.environ.get("DB_PASSWORD", "aZf5gT6f2KVb001")
-db_user = os.environ.get("DB_USER", "postgres")
+def get_secret():
+
+    secret_name = "db_credentials"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+    return secret
+
+"""
+"database-1.c5niavzkvagt.us-east-1.rds.amazonaws.com"
+"transactions"
+"aZf5gT6f2KVb001"
+"aZf5gT6f2KVb001"
+"""
+
+
+
+db_host = os.environ.get("DB_HOST")
+db_name = os.environ.get("DB_NAME")
+db_password = os.environ.get("DB_PASSWORD")
+db_user = os.environ.get("DB_USER")
 
 
 db_params = {
